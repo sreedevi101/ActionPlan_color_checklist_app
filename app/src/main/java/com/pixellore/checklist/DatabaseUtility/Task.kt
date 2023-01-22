e852c88e7ac6e7f6ff7f4c8f40acef9da144acc6
Sreedevi Sasidharan <sreedevi.s101@gmail.com>
2023-01-22 12:53:51 -0500

    Open Task editor when clicking on a task
    - Made Task and SubTask data classes parcelable
    - when task card view is clicked open Task editor activity for result
    - create a new variable getItemEditActivityResult and register it for result when opening the editor by clicking the task

--- app/src/main/java/com/pixellore/checklist/DatabaseUtility/Task.kt
+++ app/src/main/java/com/pixellore/checklist/DatabaseUtility/Task.kt
@@ -1,15 +1,52 @@
 package com.pixellore.checklist.DatabaseUtility
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.*
 
 
 @Entity(tableName = "task_table")
 data class Task(
     @PrimaryKey val task_id:Int,
-    var task_title:String,
-    var details_note:String = "",
-    var due_date:String = "",
-    var priority:String = "None",
+    var task_title: String?,
+    var details_note: String? = "",
+    var due_date: String? = "",
+    var priority: String? = "None",
     var isExpanded: Boolean = false,
     var task_isCompleted: Boolean = false
-)
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readByte() != 0.toByte(),
+        parcel.readByte() != 0.toByte()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(task_id)
+        parcel.writeString(task_title)
+        parcel.writeString(details_note)
+        parcel.writeString(due_date)
+        parcel.writeString(priority)
+        parcel.writeByte(if (isExpanded) 1 else 0)
+        parcel.writeByte(if (task_isCompleted) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Task> {
+        override fun createFromParcel(parcel: Parcel): Task {
+            return Task(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Task?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
