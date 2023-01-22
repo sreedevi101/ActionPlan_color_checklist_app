48a2708bd3d78c8872bd6aeae0f32ff27a05f937
Sreedevi Sasidharan <sreedevi.s101@gmail.com>
2023-01-22 13:08:48 -0500

    Update TaskEditorActivity.kt
    TaskEditorActivity updated to receive

--- app/src/main/java/com/pixellore/checklist/TaskEditorActivity.kt
+++ app/src/main/java/com/pixellore/checklist/TaskEditorActivity.kt
@@ -2,12 +2,14 @@ package com.pixellore.checklist
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import com.pixellore.checklist.DatabaseUtility.Subtask
 import com.pixellore.checklist.DatabaseUtility.Task
 
 class TaskEditorActivity : AppCompatActivity() {
@@ -37,6 +39,46 @@ class TaskEditorActivity : AppCompatActivity() {
         subtask2 = findViewById(R.id.subtask2)
         subtask3 = findViewById(R.id.subtask3)
         subtask4 = findViewById(R.id.subtask4)
+
+        /**
+         * Already existing Task and Subtasks passed to this Intent to edit
+         * */
+        val taskToEdit: Task?
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) { // TIRAMISU onwards
+            taskToEdit = intent.getParcelableExtra("Task", Task::class.java)
+        } else {
+            taskToEdit =intent.getParcelableExtra("Task")
+        }
+
+
+        val subtaskListSizeToEdit = intent.getIntExtra("NoOfSubtasks", 0)
+
+        val subtaskListToEdit = mutableListOf<Subtask?>()
+        var eachSubtask: Subtask?
+        for (count in 0..subtaskListSizeToEdit) {
+
+
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) { // TIRAMISU onwards
+                eachSubtask = intent.getParcelableExtra("Subtask_$count", Subtask::class.java)
+            } else {
+                eachSubtask = intent.getParcelableExtra("Subtask_$count")
+            }
+
+            subtaskListToEdit.add(eachSubtask)
+        }
+
+        /**
+         * In edit mode, Set the current Task and Subtask details to text views
+         * */
+        if (taskToEdit != null) {
+            editTaskTitleView.setText(taskToEdit.task_title)
+            editTaskDetailsView.setText(taskToEdit.details_note)
+            addDueDateView.setText(taskToEdit.due_date)
+        }
+        if (subtaskListToEdit.isNotEmpty()) {
+            // todo: fill subtasks
+        }
 
 
         val button = findViewById<Button>(R.id.button_save)
